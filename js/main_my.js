42475a35df23ce73cbd14a00c59da1041b83700c
idress4weather <danylevskainnassik@gmail.com>
2017-07-28 22:25:09 +0300

    Add files via upload

---
+++ js/main_my.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.mobile-menu').on('click', function(){
+        $('#menu').stop().slideToggle();
+    })
+});
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
